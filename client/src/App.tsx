03ad246ac03c7248addf57497c70a5a3e0537893
senadev42 <senadevv42@gmail.com>
2024-02-16 14:37:32 +0300

    added routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <></>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//Pages
+import LandingPage from './pages/LandingPage';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" Component={LandingPage} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
