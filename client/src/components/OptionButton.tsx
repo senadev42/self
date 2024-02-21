663b094699cd25b76bdc17bbe556c39e621fa29d
senadev42 <senadevv42@gmail.com>
2024-02-21 19:58:53 +0300

    index to key

--- client/src/components/OptionButton.tsx
+++ client/src/components/OptionButton.tsx
@@ -2,14 +2,14 @@ import { Option } from "./DynamicTestComponent";
 
 interface OptionButtonProps {
   isSelected: boolean;
-  index: number;
+  key: number;
   handleOptionSelect: (option: Option) => void;
   option: Option;
 }
 
 export const OptionButton = ({
   isSelected,
-  index,
+  key,
   handleOptionSelect,
   option,
 }: OptionButtonProps) => {
@@ -18,7 +18,7 @@ export const OptionButton = ({
       className={`my-2 ${
         isSelected ? "bg-pink" : "bg-blueish-100"
       } hover:bg-blueish-50 rounded-md text-white px-4 py-2 w-full h-full`}
-      key={index}
+      key={key}
       onClick={() => handleOptionSelect(option)}
     >
       {option.label}
