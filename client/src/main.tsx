da387f2a91268f38e1675b80769eded7e5097d9e
senadev42 <senadevv42@gmail.com>
2024-02-23 16:37:28 +0300

    removing strange whitespace

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
-      <div className="lg:h-screen">
+      <div className="lg:h-screen bg-cream">
         <Navbar />
         <App />
       </div>
